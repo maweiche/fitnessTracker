6076fbe859fbf735fb00c407039a7bd9a3593dde
Matt Weichel <maweiche@gmail.com>
2021-11-15 11:18:02 -0800

    fixed html routes dependencies/export

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,17 @@
-module.exports = function(app, path) {
+const express = require("express");
+const path = require("path");
+const router = require("express").Router();
     
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    app.get("/exercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-    app.get("/stats", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-};
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
